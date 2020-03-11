e15493992c527fde730a66e1be99695bb5b37ad9
32165 <3216561314@qq.com>
2020-03-11 11:00:27 +0800

    1.请求request ?a=10&b=20&c=30 1.1.视图函数  request.GET属性 解析 查询参数 QueryDict 支持一键多值 getlist('a') #查询多值 get('b') #查询单值

--- day06/DjangoLearn/brequest/urls.py
+++ day06/DjangoLearn/brequest/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     # url(r'^login_connect/(\d{4})/([a-z]+)/$',views.login_connect),
     #1.2008/beijing/拼接路径参数 直接正则匹配 给 正则起名字 获取的时候就可以不根据顺序
     url(r'^login_connect/(?P<year>\d{4})/(?P<city>[a-z]+)/$', views.login_connect),
+
+    url(r'brequest_query/$',views.brequest_query),
+
 ]
