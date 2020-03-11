767591f753c13be8d47f85885fad166a09863c39
32165 <3216561314@qq.com>
2020-03-11 16:02:29 +0800

    3.操作cookie 写入cookie request.set_cookie(key,name,time) 读取 request.COOKIES['key']

--- day06/DjangoLearn/dcookie/urls.py
+++ day06/DjangoLearn/dcookie/urls.py
@@ -2,8 +2,9 @@
 # author：yaoyao time:2020/3/11
 
 from django.conf.urls import url
+from . import views
 
 
 urlpatterns = [
-    url(),
+    url(r'^login_cookie/$',views.login_cookie,name='login_cookie'),
 ]
--- day06/DjangoLearn/dcookie/views.py
+++ day06/DjangoLearn/dcookie/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def login_cookie(request):
+    # 1.设置Cookie 1.验证身份 2.保持会话
+    response = HttpResponse('操作cookie')
+    # HttpResponse.set_cookie(cookie名，value=cookie值，max_age=cookie有效期 秒)
+    # response.set_cookie('name','tom',max_age=100)
+
+    # 获取cookie的值
+    print(request.COOKIES)
+    print(type(request.COOKIES))
+    print(request.COOKIES['name'])
+
+
+    return response
+    pass
