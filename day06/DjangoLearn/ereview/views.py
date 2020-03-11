a0b76c7a83f455a9cfd5ebbd85820f9727e77614
32165 <3216561314@qq.com>
2020-03-11 17:48:21 +0800

    4.新建ereview应用

---
+++ day06/DjangoLearn/ereview/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+# 操作session
+
+def session_response():
+
+
+    return HttpResponse("操作 session")
+
+    pass
