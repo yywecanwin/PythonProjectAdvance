154634be7db9f07ff535d37553bfd684502b85d0
32165 <3216561314@qq.com>
2020-03-13 12:13:37 +0800

    2.模板
    1.创建 template 文件夹  index.html
    2.settings ,DIRS
    3.views.py render(request,'index.html')

--- day06/DjangoLearn/btemplates/urls.py
+++ day06/DjangoLearn/btemplates/urls.py
@@ -2,7 +2,8 @@
 # author：yaoyao time:2020/3/13
 
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
-    url(),
+    url(r'^btemplate_index/',views.IndexView.as_view()),
 ]
--- day06/DjangoLearn/btemplates/views.py
+++ day06/DjangoLearn/btemplates/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic.base import View
+
+
+class IndexView(View):
+
+    def get(self,request):
+
+        # 渲染模板
+        return render(request,'djangoindex.html')
+
+        pass
+
+    def post(self):
+
+        pass
+
+    pass
