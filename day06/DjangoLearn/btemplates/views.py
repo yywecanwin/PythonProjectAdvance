91ab35497762208855182d23569ad3f36371507d
32165 <3216561314@qq.com>
2020-03-13 12:35:54 +0800

    2.模板 语法 {{ age.name.0 }} {% for item in Data %} {{ forloop.counter }} {% empty %} {% endfor %}
    {% if age > 18 %}
    
    {% endif %}

--- day06/DjangoLearn/btemplates/views.py
+++ day06/DjangoLearn/btemplates/views.py
@@ -7,9 +7,22 @@ from django.views.generic.base import View
 class IndexView(View):
 
     def get(self,request):
+        content = {
+            "name":'老王',
+            "age":88,
+            "son":[
+                "小明",
+                "小王",
+                "大头儿子"
+            ],
+            "wife":{
+                "name":"小丽",
+            }
+        }
+
 
         # 渲染模板
-        return render(request,'djangoindex.html')
+        return render(request,'djangoindex.html',context=content)
 
         pass
 
