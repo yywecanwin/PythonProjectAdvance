179c90edb05d7492fe413ffa1709246022276550
32165 <3216561314@qq.com>
2020-03-13 17:52:23 +0800

    配置mysql

--- day06/DjangoLearn/DjangoLearn/settings.py
+++ day06/DjangoLearn/DjangoLearn/settings.py
@@ -117,8 +117,13 @@ WSGI_APPLICATION = 'DjangoLearn.wsgi.application'
 # 数据库的配置
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'USER':'root',
+        'PASSWORD':'Yao199612',
+        'NAME':'django_demo',
+
     }
 }
 
